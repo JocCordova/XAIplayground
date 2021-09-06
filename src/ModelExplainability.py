ee77d059001778135c503adbea2976e9a0270123
Jose <jacordovah@gmail.com>
2021-09-06 19:12:06 +0200

    Add shap to model explainability

--- src/ModelExplainability.py
+++ src/ModelExplainability.py
@@ -3,6 +3,8 @@ import sys
 from pathlib import Path
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import shap
 import matplotlib.pyplot as plt
 from lime.lime_tabular import LimeTabularExplainer
 
@@ -40,8 +42,14 @@ class TabularExplainer:
 
     Attributes
     ----------
-    X_val : ndarray
+    X_train : ndarray
+        training data
+    y_train : ndarray
+        training labels
+    X_test : ndarray
         features to predict from
+    y_labels : {ndarray, sparse matrix}
+        decoded labels
     labels : {ndarray, sparse matrix}
         decoded labels
     clf : list of estimator
@@ -50,11 +58,10 @@ class TabularExplainer:
         pipeline used
     path : str
         dir where explanations are saved
-    explainer : explainer object
-        Lime Tabular Explainer object
+
     """
 
-    def __init__(self, classifier, pipeline, X_train, y_train, X_val, y_labels, path=GRAPH_PATH):
+    def __init__(self, classifier, pipeline, X_train, y_train, X_test, y_labels, path=GRAPH_PATH):
         """Creates Tabular Explainer
 
         Parameters
@@ -67,24 +74,23 @@ class TabularExplainer:
             training data
         y_train : ndarray
             training labels
-        X_val : ndarray
+        X_test : ndarray
             features to predict from
         y_labels : {ndarray, sparse matrix}
             decoded labels
         path : str, default="\\graphs\\explainer"
             dir where explanations are saved
         """
-        self.X_val = X_val
-        self.labels = y_labels
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_labels = y_labels
         self.clf = classifier
         self.pipeline = pipeline
         self.path = _create_explainer_dir(path)
 
-        self.explainer = LimeTabularExplainer(training_data=np.array(X_train), training_labels=y_train,
-                                              feature_names=X_train.columns, class_names=y_labels)
-
-    def explain(self, clf_index=0, data_index=0, num_features=15, savefig=True):
-        """Explains instance with a given estimator
+    def explain_lime(self, clf_index=0, data_index=0, num_features=8, suffix="", savefig=True):
+        """Explains instance with lime with a given estimator
 
         Parameters
         ----------
@@ -92,17 +98,22 @@ class TabularExplainer:
             index of estimator to explain
         data_index : int, default=0
             index of prediction to explain
-        num_features : int, default=15
+        num_features : int, default=8
             number of features to show
+        suffix : str, default=None
+            suffix for plot names
         savefig : bool, default=True
             specifies if Explanation should be saved
         """
-        X_val = self.X_val
+        X_train = self.X_train
+        y_train = self.y_train
+        X_test = self.X_test
+        y_labels = self.y_labels
         clf = self.clf[clf_index]
         pipeline = self.pipeline
         path = self.path
 
-        X_val = X_val.reset_index(drop=True)
+        X_test = X_test.reset_index(drop=True)
 
         # Create pipeline object
         pred = Predictor(clf, pipeline)
@@ -111,19 +122,110 @@ class TabularExplainer:
         model_name = str(_get_model_name(clf))
         scaler = pipeline.get_scaler_type()
 
+        # Create lime explainer
+        explainer = LimeTabularExplainer(training_data=np.array(X_train), training_labels=y_train,
+                                         feature_names=X_train.columns, class_names=y_labels)
+
         # Explain instance
-        exp = self.explainer.explain_instance(X_val.iloc[data_index], pred.predict_proba, num_features=num_features,
-                                              top_labels=3)
+        exp = explainer.explain_instance(X_test.iloc[data_index], pred.predict_proba, num_features=num_features,
+                                         top_labels=3)
         exp.show_in_notebook(show_table=True, show_all=True)
 
+        label = list((X_test.iloc[data_index], X_test.iloc[data_index]))
 
-        label = list((X_val.iloc[data_index], X_val.iloc[data_index]))
+        plt.rcParams["axes.titlesize"] = 30
+        plt.rcParams["axes.labelsize"] = 28
+        plt.rcParams["ytick.labelsize"] = 25
+        plt.rcParams["xtick.labelsize"] = 20
 
         fig = exp.as_pyplot_figure(pred.predict(label)[0])
-        fig.set_size_inches(18.5, 10.5)
+        fig.set_size_inches(15.5, 10.5)
+
+        plt.tight_layout()
+
+        if savefig:
+            exp.save_to_file(path + "\\lime_" + model_name + "_" + scaler + "_" + str(data_index) + suffix + ".html")
+            plt.savefig(path + "\\lime_" + model_name + "_" + scaler + "_" + str(data_index) + suffix + ".pdf")
 
         plt.show()
 
-        if savefig:
-            exp.save_to_file(path + "\\lime_" + model_name + "_" + scaler + "_" + str(data_index) + ".html")
-            fig.figure.savefig(path + "\\lime_" + model_name + "_" + scaler + "_" + str(data_index) + ".png")
+    def explain_shap(self, clf_index=0, n_samples=10, suffix="", class_index=None, multi_class=True, savefig=True):
+        """Explains features with shap for with a given estimator
+
+        Parameters
+        ----------
+        clf_index : int, default=0
+            index of estimator to explain
+        num_samples : int, default=10
+            number of samples to use on explainer
+        suffix : str, default=None
+            suffix for plot names
+        class_index : int or list of int, default=[1,2,0]
+            indexes of classes to explain
+        multi_class : bool, default=True
+            specifies if multi-class explanation should be created
+        savefig : bool, default=True
+            specifies if Explanation should be saved
+        """
+
+        X_train = self.X_train
+        y_labels = self.y_labels
+        X_test = self.X_test
+        clf = self.clf[clf_index]
+        pipeline = self.pipeline
+        path = self.path
+
+        if class_index is None:
+            class_index = [1, 2, 0]
+
+        # Create pipeline object
+        pred = Predictor(clf, pipeline)
+
+        # Get names for the plots
+        model_name = str(_get_model_name(clf))
+        scaler = pipeline.get_scaler_type()
+
+        X_test = X_test.reset_index(drop=True)
+
+        # Create shap explainer
+
+        explainer = shap.KernelExplainer(pred.predict_proba, data=shap.kmeans(X_train, n_samples), link="identity")
+        shap_values = explainer.shap_values(X_test)
+
+        fig = plt.figure()
+        plt.rcParams["axes.titlesize"] = 30
+        plt.rcParams["axes.labelsize"] = 28
+        plt.rcParams["ytick.labelsize"] = 25
+        plt.rcParams["xtick.labelsize"] = 20
+
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=25)
+
+        plt.tight_layout()
+
+        def savefig(class_name):
+            plt.tight_layout()
+            class_name = class_name + suffix
+
+            plt.rcParams["image.cmap"] = 'Pastel1'
+            plt.savefig(path + "\\shap_" + model_name + "_" + scaler + "_" + class_name + ".pdf")
+            plt.show()
+
+        for i in class_index:
+            class_name = y_labels[i]
+
+            shap.summary_plot(shap_values=shap_values[i], features=X_test, feature_names=X_train.columns, show=False,
+                              max_display=8)
+            plt.title("Global explanation for Class " + str(class_name))
+
+            if savefig:
+                savefig("class_" + str(class_name))
+
+        if multi_class:
+            shap.summary_plot(shap_values=shap_values, features=X_test, class_names=y_labels, show=False, max_display=8)
+            plt.title("Global explanation multi-class")
+
+            if savefig:
+                savefig("multiclass")
+
+
